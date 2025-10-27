fb0d670cf3e1af14d5a4f2681ba72228ca1beb8f
mmore500 <mmore500.login@gmail.com>
2025-10-27 18:37:38 +0000

    Deploying to binder from @ mmore500/hstrat-synthesis@aa656a75f596d3349e28ea0eaf9c651b77c161e1 🚀

---
+++ slurm/2025-10-27/2025-10-27-trafficsim_msprime.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --time=4:00:00
+#SBATCH --output="/mnt/home/%u/joblog/%j"
+#SBATCH --mail-user=mawni4ah2o@pomail.net
+#SBATCH --mail-type=FAIL,TIME_LIMIT,ARRAY_TASKS
+#SBATCH --account=beacon
+#SBATCH --requeue
+#SBATCH --array=0-8191
+
+set -euo pipefail
+
+echo "configuration ==========================================================="
+JOBDATE="$(date '+%Y-%m-%d')"
+echo "JOBDATE ${JOBDATE}"
+
+JOBNAME="$(basename -s .sh "$0")"
+echo "JOBNAME ${JOBNAME}"
+
+JOBPROJECT="$(basename -s .git "$(git remote get-url origin)")"
+echo "JOBPROJECT ${JOBPROJECT}"
+
+SOURCE_REVISION="$(git rev-parse HEAD)"
+echo "SOURCE_REVISION ${SOURCE_REVISION}"
+SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
+echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
+
+echo "initialization telemetry ==============================================="
+echo "date $(date)"
+echo "hostname $(hostname)"
+echo "PWD ${PWD}"
+echo "SLURM_JOB_ID ${SLURM_JOB_ID:-nojid}"
+echo "SLURM_ARRAY_TASK_ID ${SLURM_ARRAY_TASK_ID:-notid}"
+module purge || :
+module load Python/3.10.8 || :
+echo "python3.10 $(which python3.10)"
+echo "python3.10 --version $(python3.10 --version)"
+
+echo "setup HOME dirs ========================================================"
+mkdir -p "${HOME}/joblatest"
+mkdir -p "${HOME}/joblog"
+mkdir -p "${HOME}/jobscript"
+if ! [ -e "${HOME}/scratch" ]; then
+    if [ -e "/mnt/scratch/${USER}" ]; then
+        ln -s "/mnt/scratch/${USER}" "${HOME}/scratch" || :
+    else
+        mkdir -p "${HOME}/scratch" || :
+    fi
+fi
+
+echo "launch job ============================================================="
+apptainer exec docker://ghcr.io/mmore500/hstrat-synthesis@sha256:70445f4dc3b41a137d4f79284feaa045f7ff8f0d802518cbdf53a05bbab86b74 python3 -m pylib.trafficsim_msprime "$(( SLURM_ARRAY_TASK_ID % 2 == 0 ? 32 : 16 ))"
