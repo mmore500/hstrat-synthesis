0f44b58de63f4c74d91c558e8d85f429a8907710
mmore500 <mmore500.login@gmail.com>
2026-02-13 12:15:24 +0000

    Deploying to binder-2024-10-20-example.ipynb from @ mmore500/hstrat-synthesis@51fef838f5bdf21f98c4270ce0137de49b3bc8fe 🚀

--- pylib/track_ca.py
+++ pylib/track_ca.py
@@ -23,14 +23,17 @@ import tqdm
 np.bool = bool
 
 
-class MultiTracker():
+class MultiTracker:
 
     _supported_S = {1, 2, 4, 8, 16, 32, 64}
 
     def _get_int_dtype(self):
-        if self.bitwidth >= 64: return self.np.uint64
-        if self.bitwidth >= 32: return self.np.uint32
-        if self.bitwidth >= 16: return self.np.uint16
+        if self.bitwidth >= 64:
+            return self.np.uint64
+        if self.bitwidth >= 32:
+            return self.np.uint32
+        if self.bitwidth >= 16:
+            return self.np.uint16
         return self.np.uint8
 
     def __init__(
@@ -40,9 +43,11 @@ class MultiTracker():
         dstream_algo: types.ModuleType,
         *,
         N: int = 1,
-        backend: types.ModuleType = np
+        backend: types.ModuleType = np,
     ):
-        assert dstream_S in self._supported_S, f"dstream_S must be in {self._supported_S}"
+        assert (
+            dstream_S in self._supported_S
+        ), f"dstream_S must be in {self._supported_S}"
         self.algo = dstream_algo
         self.S = dstream_S
         self.bitwidth = dstream_bitwidth
@@ -64,7 +69,10 @@ class MultiTracker():
         self.R = R
 
         self.hst_markers = self.np.random.randint(
-            0, 2**self.bitwidth, size=(self.N, H, W, self.S), dtype=self.marker_dtype
+            0,
+            2**self.bitwidth,
+            size=(self.N, H, W, self.S),
+            dtype=self.marker_dtype,
         )  # deposit random stuff everywhere first gen
 
         self.ancestor_search = [
@@ -99,26 +107,43 @@ class MultiTracker():
             arr = self.np.zeros_like(data, dtype=self.np.float16)
             arr[*self._get_slices(-x, -y)] = parent[*self._get_slices(x, y)]
             ancestor_matrices.append(arr[None])
-        scores = self.np.concatenate(ancestor_matrices)[None].repeat(self.N, axis=0)
+        scores = self.np.concatenate(ancestor_matrices)[None].repeat(
+            self.N, axis=0
+        )
 
         if add_random_noise:
             if self.N == 1 and self.t == 1:  # only print this once
                 print("Warning: It does not make sense to add noise with N=1")
-            scores += self.rng.uniform(low=0, high=0.5, size=scores.shape).astype(self.np.float16)  # 0.5 to ensure 0's don't beat 1's
+            scores += self.rng.uniform(
+                low=0, high=0.5, size=scores.shape
+            ).astype(
+                self.np.float16
+            )  # 0.5 to ensure 0's don't beat 1's
 
-        winning_scores = self.parents.transpose(1, 2, 0)[None].repeat(self.N, axis=0)[
-            self.np.eye(scores.shape[1], dtype=self.np.bool_)[scores.argmax(axis=1)]
-        ].reshape(self.N, self.H, self.W)
+        winning_scores = (
+            self.parents.transpose(1, 2, 0)[None]
+            .repeat(self.N, axis=0)[
+                self.np.eye(scores.shape[1], dtype=self.np.bool_)[
+                    scores.argmax(axis=1)
+                ]
+            ]
+            .reshape(self.N, self.H, self.W)
+        )
         self.hst_markers[:] = self.hst_markers[
-            self.np.arange(self.hst_markers.shape[0]).reshape(self.hst_markers.shape[0], 1, 1),
+            self.np.arange(self.hst_markers.shape[0]).reshape(
+                self.hst_markers.shape[0], 1, 1
+            ),
             winning_scores // self.W,
-            winning_scores % self.W
+            winning_scores % self.W,
         ]
 
         item_to_assign = self.algo.assign_storage_site(self.S, self.t)
         if item_to_assign is not None:
             self.hst_markers[..., item_to_assign] = self.np.random.randint(
-                0, 2**self.bitwidth, size=(self.N, self.H, self.W), dtype=self.marker_dtype
+                0,
+                2**self.bitwidth,
+                size=(self.N, self.H, self.W),
+                dtype=self.marker_dtype,
             )
         if save:
             self.history.append((self.t, data.copy(), self.hst_markers.copy()))
@@ -141,8 +166,9 @@ class MultiTracker():
                     self.to_numpy(self.np.argwhere(ca_state)),
                     np.apply_along_axis(
                         self._pack_hex,
-                        -1, self.to_numpy(hst_markers[idx, ca_state])
-                    )
+                        -1,
+                        self.to_numpy(hst_markers[idx, ca_state]),
+                    ),
                 )
                 for t, ca_state, hst_markers in self.history
             ]
@@ -154,7 +180,10 @@ class MultiTracker():
                     population.append(
                         {
                             "downstream_version": downstream.__version__,
-                            "data_hex": np.asarray(t, dtype=">u4").tobytes().hex() + i,
+                            "data_hex": np.asarray(t, dtype=">u4")
+                            .tobytes()
+                            .hex()
+                            + i,
                             "dstream_algo": f"dstream.{self.algo.__name__.split('.')[-1]}",
                             "dstream_storage_bitoffset": 32,
                             "dstream_storage_bitwidth": self.S * self.bitwidth,
@@ -164,12 +193,16 @@ class MultiTracker():
                             "extant": t == self.t,
                             "row": y,
                             "col": x,
-                            "state": 1
+                            "state": 1,
                         }
                     )
 
-            postprocessor = AssignOriginTimeNodeRankTriePostprocessor(t0="dstream_S")
-            df = surface_unpack_reconstruct(pl.from_pandas(pd.DataFrame(population))).to_pandas()
+            postprocessor = AssignOriginTimeNodeRankTriePostprocessor(
+                t0="dstream_S"
+            )
+            df = surface_unpack_reconstruct(
+                pl.from_pandas(pd.DataFrame(population))
+            ).to_pandas()
             df["rank"] = df["hstrat_rank"]
             df = postprocessor(df, 2**-1)
             results.append(df)
@@ -190,18 +223,18 @@ class MultiTracker():
         return packed_bytes.hex()
 
 
-
 class CATracker(MultiTracker):
-
     def __init__(
         self,
         dstream_S: int,
         dstream_bitwidth: int,
         dstream_algo: types.ModuleType,
         *,
-        backend: types.ModuleType = np
+        backend: types.ModuleType = np,
     ):
-        super().__init__(dstream_S, dstream_bitwidth, dstream_algo, N=1, backend=backend)
+        super().__init__(
+            dstream_S, dstream_bitwidth, dstream_algo, N=1, backend=backend
+        )
 
     def reconstruct_phylogeny(self, *, verbose=False):
         df = self.reconstruct_phylogenies(verbose=verbose)[0]
@@ -241,6 +274,7 @@ def track_ca_history(
     else:
         try:
             import cupy as cp
+
             if isinstance(data, cp.ndarray):
                 backend = cp
             else:
@@ -252,25 +286,38 @@ def track_ca_history(
         dstream_S=dstream_S,
         dstream_algo=dstream_algo,
         dstream_bitwidth=dstream_bitwidth,
-        backend=backend
+        backend=backend,
     )
     ca_tracker.initialize(data[0], R=R)
-    for i, curr in tqdm.tqdm(enumerate(data[1:], start=1), total=data.shape[0]-1):
-        ca_tracker.step(curr, save=i in fossil_range, add_random_noise=add_random_noise)
+    for i, curr in tqdm.tqdm(
+        enumerate(data[1:], start=1), total=data.shape[0] - 1
+    ):
+        ca_tracker.step(
+            curr, save=i in fossil_range, add_random_noise=add_random_noise
+        )
     return ca_tracker.reconstruct_phylogeny()
 
 
-def plot_phylo_at(phylo_df: pd.DataFrame, teeplot_subdir: str, show: bool = True, fossil_rank: typing.Optional[int] = None) -> None:
+def plot_phylo_at(
+    phylo_df: pd.DataFrame,
+    teeplot_subdir: str,
+    show: bool = True,
+    fossil_rank: typing.Optional[int] = None,
+) -> None:
     if fossil_rank is None:
         fossil_rank = phylo_df["hstrat_rank"].max() + 1
     df = phylo_df[phylo_df["hstrat_rank"] < fossil_rank].copy()
-    df.loc[(df["hstrat_rank"] == fossil_rank - 1) & (df["state"] == 1), "extant"] = True
+    df.loc[
+        (df["hstrat_rank"] == fossil_rank - 1) & (df["state"] == 1), "extant"
+    ] = True
     del df["origin_time"]
     dp_tree = apc.alife_dataframe_to_dendropy_trees(
         hstrat._auxiliary_lib.alifestd_try_add_ancestor_list_col(df),
         setup_edge_lengths=True,
     )
-    grid_dim = int(df[df["gol_state"] >= 0][["row", "col"]].to_numpy().max()) + 1
+    grid_dim = (
+        int(df[df["gol_state"] >= 0][["row", "col"]].to_numpy().max()) + 1
+    )
 
     with tp.teed(
         plt.subplots,
@@ -278,15 +325,14 @@ def plot_phylo_at(phylo_df: pd.DataFrame, teeplot_subdir: str, show: bool = True
         2,
         figsize=(10, 10),
         gridspec_kw={
-            'width_ratios': [0.2, 0.8],
-            'height_ratios': [1],
-            'wspace': 0.05,
-            'hspace': 0.05,
+            "width_ratios": [0.2, 0.8],
+            "height_ratios": [1],
+            "wspace": 0.05,
+            "hspace": 0.05,
         },
         teeplot_subdir=teeplot_subdir,
         teeplot_outattrs={"rank": fossil_rank},
-        teeplot_show=show
-
+        teeplot_show=show,
     ) as teed:
         fig, (ax_left, ax_grid) = teed
 
@@ -322,7 +368,8 @@ def plot_phylo_at(phylo_df: pd.DataFrame, teeplot_subdir: str, show: bool = True
         for idx, row in df[df["is_leaf"] & df["extant"]].iterrows():
 
             tree_x, tree_y = next(
-                v for n, v in tree_left.get_layout().T.items()
+                v
+                for n, v in tree_left.get_layout().T.items()
                 if n.taxon is not None and n.taxon.label == row["id"]
             )
             grid_x, grid_y = row["col"], row["row"]
@@ -342,12 +389,24 @@ def plot_phylo_at(phylo_df: pd.DataFrame, teeplot_subdir: str, show: bool = True
             fig.add_artist(con)
 
 
-def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: str, child_rank: typing.Optional[int] = None, show: bool = False) -> None:
+def plot_ancestry_at(
+    phylo_df: pd.DataFrame,
+    parent_rank: int,
+    teeplot_subdir: str,
+    child_rank: typing.Optional[int] = None,
+    show: bool = False,
+) -> None:
     if child_rank is None:
         child_rank = phylo_df["hstrat_rank"].max() + 1
     df_by_id = phylo_df.set_index("id", inplace=False)
     parent_taxa, child_taxa = (
-        set(phylo_df.loc[(phylo_df["hstrat_rank"] == rank - 1) & (phylo_df["state"] == 1), "taxon_label"])
+        set(
+            phylo_df.loc[
+                (phylo_df["hstrat_rank"] == rank - 1)
+                & (phylo_df["state"] == 1),
+                "taxon_label",
+            ]
+        )
         for rank in (parent_rank, child_rank)
     )
     dp_tree = apc.alife_dataframe_to_dendropy_trees(
@@ -357,18 +416,31 @@ def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: s
 
     df_no_origin_time = phylo_df.drop(columns=["origin_time"])
     dp_tree_for_plotting = apc.alife_dataframe_to_dendropy_trees(
-        hstrat._auxiliary_lib.alifestd_try_add_ancestor_list_col(df_no_origin_time),
+        hstrat._auxiliary_lib.alifestd_try_add_ancestor_list_col(
+            df_no_origin_time
+        ),
         setup_edge_lengths=True,
     )
     pdm = dp_tree[0].phylogenetic_distance_matrix()
 
     # determine the most recent parent for each child
     ancestors = [
-        max((
-            (parent_id, df_by_id.at[pdm.mrca(child_id, parent_id).taxon.label, "hstrat_rank"])
-            for parent_id in dp_tree[0].taxon_namespace if parent_id.label in parent_taxa
-        ), key=lambda x: x[1])[0].label
-        for child_id in dp_tree[0].taxon_namespace if child_id.label in child_taxa
+        max(
+            (
+                (
+                    parent_id,
+                    df_by_id.at[
+                        pdm.mrca(child_id, parent_id).taxon.label,
+                        "hstrat_rank",
+                    ],
+                )
+                for parent_id in dp_tree[0].taxon_namespace
+                if parent_id.label in parent_taxa
+            ),
+            key=lambda x: x[1],
+        )[0].label
+        for child_id in dp_tree[0].taxon_namespace
+        if child_id.label in child_taxa
     ]
 
     with tp.teed(
@@ -377,17 +449,17 @@ def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: s
         2,
         figsize=(20, 12),
         gridspec_kw={
-            'width_ratios': [0.5, 0.5],
-            'height_ratios': [0.2, 0.8],
-            'wspace': 0.05,
-            'hspace': 0.05,
+            "width_ratios": [0.5, 0.5],
+            "height_ratios": [0.2, 0.8],
+            "wspace": 0.05,
+            "hspace": 0.05,
         },
         teeplot_subdir=teeplot_subdir,
         teeplot_outattrs={
             "parent-rank": parent_rank,
-            "child-rank": child_rank
+            "child-rank": child_rank,
         },
-        teeplot_show=show
+        teeplot_show=show,
     ) as teed:
 
         fig, ((ax_top1, ax_top2), (ax_left, ax_right)) = teed
@@ -407,13 +479,27 @@ def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: s
         ax_top.margins(x=-0.04)
         ax_top.set_xlim(ax_top.get_xlim()[0] - 10, None)
 
-
-        for i, (rank, ax) in enumerate([(parent_rank, ax_left), (child_rank, ax_right)]):
+        for i, (rank, ax) in enumerate(
+            [(parent_rank, ax_left), (child_rank, ax_right)]
+        ):
 
             # create grid of the CA cells
-            grid_dim = int(phylo_df[(phylo_df["hstrat_rank"] == rank - 1) & (phylo_df["gol_state"] >= 0)][["row", "col"]].to_numpy().max()) + 1
+            grid_dim = (
+                int(
+                    phylo_df[
+                        (phylo_df["hstrat_rank"] == rank - 1)
+                        & (phylo_df["gol_state"] >= 0)
+                    ][["row", "col"]]
+                    .to_numpy()
+                    .max()
+                )
+                + 1
+            )
             grid = np.full((grid_dim, grid_dim), np.nan)
-            for _, row in phylo_df[(phylo_df["hstrat_rank"] == rank - 1) & (phylo_df["gol_state"] >= 0)].iterrows():
+            for _, row in phylo_df[
+                (phylo_df["hstrat_rank"] == rank - 1)
+                & (phylo_df["gol_state"] >= 0)
+            ].iterrows():
                 grid[int(row["row"]), int(row["col"])] = 1
 
             # plot heatmap
@@ -422,7 +508,9 @@ def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: s
                 ax=ax,
                 vmin=1,
                 vmax=1,
-                cmap=ListedColormap(sns.color_palette(["#0099ff" if i == 1 else "#ff0000"])),
+                cmap=ListedColormap(
+                    sns.color_palette(["#0099ff" if i == 1 else "#ff0000"])
+                ),
                 cbar=False,
             )
             # ax.set_axis_off()
@@ -430,13 +518,17 @@ def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: s
             ax.set_yticks([])
             ax.set_xticklabels([])
             ax.set_yticklabels([])
-            ax.patch.set_edgecolor('black')
+            ax.patch.set_edgecolor("black")
             ax.patch.set_linewidth(1)
 
-            for _, row in phylo_df[(phylo_df["hstrat_rank"] == rank - 1) & (phylo_df["gol_state"] >= 0)].iterrows():
+            for _, row in phylo_df[
+                (phylo_df["hstrat_rank"] == rank - 1)
+                & (phylo_df["gol_state"] >= 0)
+            ].iterrows():
 
                 tree_x, tree_y = next(
-                    v for n, v in tree.get_layout().T.items()
+                    v
+                    for n, v in tree.get_layout().T.items()
                     if n.taxon is not None and n.taxon.label == row["id"]
                 )
                 grid_x, grid_y = row["col"], row["row"]
@@ -455,9 +547,15 @@ def plot_ancestry_at(phylo_df: pd.DataFrame, parent_rank: int, teeplot_subdir: s
                 )
                 fig.add_artist(con)
 
-        for (child, parent) in zip(child_taxa, ancestors):
-            child_y, child_x = df_by_id.at[child, "row"], df_by_id.at[child, "col"]
-            parent_y, parent_x = df_by_id.at[parent, "row"], df_by_id.at[parent, "col"]
+        for child, parent in zip(child_taxa, ancestors):
+            child_y, child_x = (
+                df_by_id.at[child, "row"],
+                df_by_id.at[child, "col"],
+            )
+            parent_y, parent_x = (
+                df_by_id.at[parent, "row"],
+                df_by_id.at[parent, "col"],
+            )
             con = ConnectionPatch(
                 xyA=(parent_x, parent_y),
                 xyB=(child_x, child_y),
