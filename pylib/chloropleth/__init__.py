3183ff498f6d1ed60ead41287b04c9d90baf15f3
mmore500 <mmore500.login@gmail.com>
2026-03-03 21:55:30 +0000

    Deploying to binder-2025-10-27-trafficsim_msprime.ipynb from @ mmore500/hstrat-synthesis@86e571785218874ce3401d74cf6a8f9aa5833a64 🚀

---
+++ pylib/chloropleth/__init__.py
@@ -0,0 +1,5 @@
+from ._draw_cbar import draw_cbar
+from ._draw_cgrid import draw_cgrid
+from ._draw_cscatter import draw_cscatter
+
+__all__ = ["draw_cbar", "draw_cgrid", "draw_cscatter"]
---
+++ pylib/chloropleth/_draw_cscatter.py
@@ -0,0 +1,101 @@
+import itertools as it
+import types
+import typing
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from ._draw_cgrid import draw_cgrid
+
+
+def draw_cscatter(
+    data: pd.DataFrame,
+    *,
+    x: str,
+    y: str,
+    cmap: typing.Callable,
+    ax: typing.Optional[mpl.axes.Axes] = None,
+    border: bool = False,
+    despine: bool = True,
+    major: typing.Optional[float] = None,
+    minor: typing.Optional[float] = None,
+    rect_kws: dict = types.MappingProxyType({}),
+    scatter_kws: dict = types.MappingProxyType({}),
+    xmax: float = 1.0,
+    ymax: float = 1.0,
+) -> mpl.axes.Axes:
+
+    ax = draw_cgrid(
+        ax=ax, cmap=cmap, xmax=xmax, ymax=ymax, imshow_kws=dict(zorder=-300)
+    )
+    rect = mpl.patches.Rectangle(
+        (0, 0),
+        1,
+        1,
+        transform=ax.transAxes,
+        **{
+            **dict(
+                alpha=0.8,
+                facecolor="white",
+                zorder=-200,
+            ),
+            **rect_kws,
+        },
+    )
+    ax.add_patch(rect)
+
+    ax = sns.scatterplot(
+        data=data,
+        x=x,
+        y=y,
+        ax=ax,
+        color=[*it.starmap(cmap, zip(data[x] / xmax, data[y] / ymax))],
+        **{
+            **dict(
+                alpha=0.3,
+                clip_on=False,
+                edgecolor=None,
+                legend=False,
+                linewidth=0,
+                s=100,
+                zorder=-100,
+            ),
+            **scatter_kws,
+        },
+    )
+
+    if minor is not None:
+        ax.grid(visible=True, which="minor", color="gray", ls="--", lw=0.2)
+        ax.xaxis.set_minor_locator(plt.MultipleLocator(minor))
+        ax.yaxis.set_minor_locator(plt.MultipleLocator(minor))
+
+    if major is not None:
+        ax.grid(visible=True, which="major", color="#222222", lw=0.4)
+        ax.xaxis.set_major_locator(plt.MultipleLocator(major))
+        ax.yaxis.set_major_locator(plt.MultipleLocator(major))
+
+    if despine:
+        ax.tick_params(axis="both", which="both", length=0)
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+        sns.despine(ax=ax, left=True, bottom=True)
+        if major is not None:
+            for spine in "left", "bottom":
+                ax.spines[spine].set_visible(True)
+                ax.spines[spine].set_color("#222222")
+                ax.spines[spine].set_linewidth(0.4)
+
+    if border:
+        for spine in ax.spines.values():
+            spine.set_visible(True)
+            spine.set_color("#222222")
+            spine.set_linewidth(0.4)
+
+    ax.set_xlim(0, xmax)
+    ax.set_ylim(0, ymax)
+
+    return ax
