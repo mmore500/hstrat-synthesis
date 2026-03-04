ff5b1b4e2ad6bab094ac741a4a72991b2c7cd375
mmore500 <mmore500.login@gmail.com>
2026-03-04 02:29:41 +0000

    Deploying to binder-2025-10-28-trafficsim_msprime.ipynb from @ mmore500/hstrat-synthesis@b7486dfee99cf7d85989f7e485269cf4ecb39f35 🚀

--- pylib/chloropleth/__init__.py
+++ pylib/chloropleth/__init__.py
@@ -1,5 +1,11 @@
 from ._draw_cbar import draw_cbar
 from ._draw_cgrid import draw_cgrid
 from ._draw_cscatter import draw_cscatter
+from ._draw_ctree import draw_ctree
 
-__all__ = ["draw_cbar", "draw_cgrid", "draw_cscatter"]
+__all__ = [
+    "draw_cbar",
+    "draw_cgrid",
+    "draw_cscatter",
+    "draw_ctree",
+]
