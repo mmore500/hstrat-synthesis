fa2a8146abe56176258e7db7ad2d62ddfdf7fda4
mmore500 <mmore500@users.noreply.github.com>
2026-03-04 22:25:22 +0000

    Deploying to binder-2026-02-11-billion-tip-benchmark-v1_20_24.ipynb from @ mmore500/hstrat-synthesis@7454552ddb15998cdd2d1c96280cb04bd7d87ab8 🚀

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -2,11 +2,7 @@ __author__ = "Matthew Andres Moreno"
 __email__ = "m.more500@gmail.com"
 __version__ = "0.0.0"
 
-from . import (
-    chloropleth,
-    cmap,
-    tree,
-)
+from . import chloropleth, cmap, tree
 from ._read_parquet_with_retry import (
     read_csv_with_retry,
     read_parquet_with_retry,
